9e99e005f2002672de8805be4d308a30d58fcee7
GG <raki1000000@mail.ru>
2026-06-18 10:14:39 +0200

    Initial Commit

---
+++ MyFocus/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MyFocus
+//
+//  Created by gg on 18.06.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
